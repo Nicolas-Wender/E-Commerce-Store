0100ee059a0576228bebe227d187f58f8c66f2b3
Nicolas Wender Fontes Queiroz <nicolas.w3nder@gmail.com>
2023-12-26 17:54:19 -0300

    feat: Implementando listagem de produtos

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,14 +1,20 @@
 import getBillboard from '@/actions/get-billboard'
+import getProducts from '@/actions/get-products'
 import BillBoard from '@/components/BillBoard'
+import ProductList from '@/components/ProductList'
 import Container from '@/components/ui/Container'
 
 export default async function Home() {
+  const products = await getProducts({ isFeatured: true })
   const billboard = await getBillboard('707432ef-d96f-46de-9d99-786cae80c769')
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <BillBoard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   )
