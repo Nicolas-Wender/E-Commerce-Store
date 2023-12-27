86c02c5c19d53e86a6922fbd8d98a2b673a38de1
Nicolas Wender Fontes Queiroz <nicolas.w3nder@gmail.com>
2023-12-26 23:42:37 -0300

    feat: adicionando página individual de categorias

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -12,12 +12,12 @@ interface MainNavProps {
 export default function MainNav({ data }: MainNavProps) {
   const pathname = usePathname()
 
-
   const routes = data.map(route => ({
     href: `/category/${route.id}`,
     label: route.name,
     active: pathname === `/category/${route.id}`
   }))
+
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes.map(route => (
